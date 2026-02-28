520135d7770294247b19d13daf2a0d0c6362fcef
Aaron Harper <aaron@inngest.com>
2026-02-27 22:27:30 -0500

    Improve mdx LSP

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,6 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 
+-- Treat MDX files as markdown for better LSP support
+vim.filetype.add({
+  extension = {
+    mdx = "markdown",
+  },
+})
+
 -- Keep default register separate from system clipboard.
 -- Use "+y / "+p to explicitly access the system clipboard.
 vim.opt.clipboard = ""
@@ -11,6 +18,10 @@ vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold" }, {
   command = "silent! checktime",
 })
 local timer = vim.uv.new_timer()
-timer:start(0, 1000, vim.schedule_wrap(function()
-  pcall(vim.cmd, "silent! checktime")
-end))
+timer:start(
+  0,
+  1000,
+  vim.schedule_wrap(function()
+    pcall(vim.cmd, "silent! checktime")
+  end)
+)
