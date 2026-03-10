dcd137aff078aa1633fb31fab187bd4b372d9dd3
Aaron Harper <aaron@inngest.com>
2026-03-10 12:51:17 -0400

    Improve session picker

--- tmux/session-picker.sh
+++ tmux/session-picker.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 # Fuzzy-find tmux sessions sorted by most recently used.
 # Bound to <prefix> C-f in .tmux.conf.
+current=$(tmux display-message -p '#S')
 tmux list-sessions -F '#{session_last_attached} #{session_name}' \
   | LC_ALL=C sort -rn \
   | cut -d' ' -f2- \
+  | grep -xv "$current" \
   | fzf --reverse \
   | xargs -I{} tmux switch-client -t '{}'
