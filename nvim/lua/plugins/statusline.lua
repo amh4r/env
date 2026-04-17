d1ae9ff284cb930059f8301ee4f94eae91ede99d
Aaron Harper <aaron@inngest.com>
2026-04-17 08:56:25 -0400

    Show more path in nvim

--- nvim/lua/plugins/statusline.lua
+++ nvim/lua/plugins/statusline.lua
@@ -4,6 +4,9 @@ return {
     opts = function(_, opts)
       -- Hide cursor position (line:col) from the right side
       opts.sections.lualine_z = {}
+
+      -- Show the full relative file path instead of truncating to 3 segments
+      opts.sections.lualine_c[4] = { LazyVim.lualine.pretty_path({ length = 0 }) }
     end,
   },
 }
